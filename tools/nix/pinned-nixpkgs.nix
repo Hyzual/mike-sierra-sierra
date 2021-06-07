5f9d903959d252d5733b7e2ee247d60c5ca15894
Hyzual <hyzual@gmail.com>
2021-06-07 22:23:27 +0200

    Bump nixpkgs
    go 1.16.4 -> 1.16.5
    npm 7.11.1 -> 7.15.1

--- tools/nix/pinned-nixpkgs.nix
+++ tools/nix/pinned-nixpkgs.nix
@@ -1,4 +1,4 @@
 {}:
 
-let pinnedNixPkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5dbd28d75410738ee7a948c7dec9f9cb5a41fa9d.tar.gz") {};
+let pinnedNixPkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ffb7cfcfad15e3bff9d05336767e59ee6ee24cb6.tar.gz") {};
 in pinnedNixPkgs
